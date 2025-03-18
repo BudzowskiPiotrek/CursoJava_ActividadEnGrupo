5d6dc5563546ea455c42e9be0fc76d9e65eda7c6
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-18 18:29:26 +0100

    Update EstadoAsignatura.java

--- actividadFinal/src/proyecto/EstadoAsignatura.java
+++ actividadFinal/src/proyecto/EstadoAsignatura.java
@@ -1,6 +1,14 @@
 package proyecto;
 
+/**
+ * Enumeración que representa el estado de una asignatura.
+ * @author [María Fernanda]
+ */
 public enum EstadoAsignatura {
-	SUPERADO, CURSADO, ABANDONADO
-
+	/** La asignatura ha sido superada. */
+	SUPERADO,
+	/** La asignatura está siendo cursada actualmente. */
+	CURSADO,
+	/** La asignatura ha sido abandonada. */
+	ABANDONADO
 }
