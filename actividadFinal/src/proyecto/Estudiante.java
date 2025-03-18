48602aceabedcb5154db1d81965ebf286069c8f3
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-18 08:56:27 +0100

    Update Estudiante.java

--- actividadFinal/src/proyecto/Estudiante.java
+++ actividadFinal/src/proyecto/Estudiante.java
@@ -1,5 +1,31 @@
 package proyecto;
 
+import java.util.Map;
+
 public class Estudiante extends Persona {
+	private Map<Asignatura, EstadoAsignatura> asignatura;
+
+	public Estudiante(String nombre, Map<Asignatura, EstadoAsignatura> asignatura) {
+		super(nombre);
+		this.asignatura = asignatura;
+	}
+
+	public Map<Asignatura, EstadoAsignatura> getAsignatura() {
+		return asignatura;
+	}
+
+	public void setAsignatura(Map<Asignatura, EstadoAsignatura> asignatura) {
+		this.asignatura = asignatura;
+	}
+
+	public boolean borrarAsignatura() {
+		boolean exito = false;
+		return exito;
+	}
+
+	public boolean mostrarAsignaturas() {
+		boolean exito = false;
+		return exito;
+	}
 
 }
