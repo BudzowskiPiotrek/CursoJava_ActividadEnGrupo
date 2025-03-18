ca89c64a56863d7015b47b1b00b5f49de766d1d6
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-18 09:43:35 +0100

    Update EstadoAsignatura.java

--- actividadFinal/src/proyecto/EstadoAsignatura.java
+++ actividadFinal/src/proyecto/EstadoAsignatura.java
@@ -1,25 +1,6 @@
 package proyecto;
 
-import java.util.Iterator;
-
 public enum EstadoAsignatura {
 	SUPERADO, CURSADO, ABANDONADO
 
-	
-	public static void eliminarEstudiante() {
-		System.out.println("Introduce el nombre del estudiante");
-		String nombre = letra.nextLine().toUpperCase();
-				
-		Iterator<Persona> iterator = persona.iterator();
-		while (iterator.hasNext()) {
-			Persona persona = iterator.next();
-			if (persona.getNombre().equalsIgnoreCase(nombre)) {
-				iterator.remove();
-				System.out.println("Borrado con exito");
-				return;
-			}
-		}
-		System.out.println("No se encontro al estudiante");
-		
-	}
 }
